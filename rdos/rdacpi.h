14b42b9d80ad63ea9b1e82112913fe650c8e7a65
rdos314 <leif@rdos.net>
2025-06-02 21:04:40 +0200

    Add message interface.

--- rdos/rdacpi.h
+++ rdos/rdacpi.h
@@ -5,6 +5,11 @@
 #define serv_gate_uacpi_disable_io 0x00000005
 #define serv_gate_uacpi_start_pci 0x00000006
 #define serv_gate_uacpi_has_apic 0x00000007
+#define serv_gate_uacpi_wait_for_cmd 0x00000008
+#define serv_gate_uacpi_reply_cmd 0x00000009
+#define serv_gate_uacpi_reply_data_cmd 0x0000000A
+
+
 #define ServGate_uacpi_get_acpi 0x55 0x67 0x9a 1 0 0 0 4 0 0x5d
 #define ServGate_uacpi_map 0x55 0x67 0x9a 2 0 0 0 4 0 0x5d
 #define ServGate_uacpi_unmap 0x55 0x67 0x9a 3 0 0 0 4 0 0x5d
@@ -12,3 +17,8 @@
 #define ServGate_uacpi_disable_io 0x55 0x67 0x9a 5 0 0 0 4 0 0x5d
 #define ServGate_uacpi_start_pci 0x55 0x67 0x9a 6 0 0 0 4 0 0x5d
 #define ServGate_uacpi_has_apic 0x55 0x67 0x9a 7 0 0 0 4 0 0x5d
+#define ServGate_uacpi_wait_for_cmd 0x55 0x67 0x9a 8 0 0 0 4 0 0x5d
+#define ServGate_uacpi_reply_cmd 0x55 0x67 0x9a 9 0 0 0 4 0 0x5d
+#define ServGate_uacpi_reply_data_cmd 0x55 0x67 0x9a 10 0 0 0 4 0 0x5d
+
+
